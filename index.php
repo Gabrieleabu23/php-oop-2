8da711f99a34c0cb0a856ca18f6361b10609a9df
Gabrieleabu23 <kaidgabriele23@gmail.com>
2024-02-05 15:04:57 +0100

    add: structure files for exercise

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Animal Food</title>
+</head>
+<body>
+    <?php
+    // DEBUG
+    // echo "HELLO WORLD";
+    
+    ?>
+</body>
+</html>
